6fa14bccc31623edd2d9383256de81deabbdc6d4
tki9ee <tki9ee@AHCL12822.local>
2021-02-10 14:13:31 -0500

    Test Commit

--- Advent2019/src/TestClass.java
+++ Advent2019/src/TestClass.java
@@ -3,14 +3,6 @@ import java.util.List;
 
 public class TestClass {
 	public static void main(String[] args) {
-		int intPosition1 = 1;
-		System.out.println(intPosition1 % 10);
-		intPosition1  = intPosition1 / 10;
-		System.out.println(intPosition1 % 10);
-		intPosition1  = intPosition1 / 10;
-		System.out.println(intPosition1 % 10);
-		intPosition1  = intPosition1 / 10;
-		System.out.println(intPosition1 % 10);
-		intPosition1  = intPosition1 / 10;
+		
 	}
 }
